1007dccf8aa06c7fa9a790e3ce0ae9c89ea3e9ba
Lucas Fayolle <lfayolle1@gmail.com>
2016-04-11 16:20:26 +0200

    big cleanup; setup correct dev environement

--- register.php
+++
@@ -1,7 +0,0 @@
-<?php 
-  $file = dirname(__FILE__).'/user-data-'.time().'-'.rand(1000,9999);
-  $newURL = 'http://union.dev.com?register=success';
-  file_put_contents($file, json_encode($_REQUEST));
-  header('Location: '.$newURL);
-  die();
-?>
