0ae69ff6350aa0b4a1a963e68b04fcd301be6b2e
Lucas Fayolle <lfayolle1@gmail.com>
2016-04-13 21:22:03 +0200

    implement slick + custom prev/nexnext animation

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -10,10 +10,12 @@ $(document).ready(function(){
 
 function initPage(){
 
-	hideLoader();
+	
+	setTimeout(initSlider, 200);
+	setTimeout(hideLoader, 250);
 
-	if ( $('.isMobile').is(':visible') ) {
-	}
+	// if ( $('.isMobile').is(':visible') ) {
+	// }
 }
 
 function hideLoader() {
@@ -26,4 +28,67 @@ function hideLoader() {
 
 function scrollTopFn() {
 	TweenMax.to(window, 1.5, {scrollTo:0,ease:Power3.easeOut});
+}
+
+function initSlider() {
+	var beforeChangeTl = new TimelineMax({paused:true});
+	var afterChangeTl = new TimelineMax({paused:true});
+	var textes, image;
+
+	// When init, set first slide visible : 
+	$('.slide-wrap').on('init', function(slick){
+		var slide = $('.slide:eq(0)');
+
+		image = slide.find('img');
+		text = slide.find('h4, p');
+
+		afterChangeTl
+			.staggerFromTo(text, 0.25, {y:50,opacity:0}, {y:0,opacity:1,ease:Expo.easeOut}, 0.1)
+			.fromTo(image, 0.35, {left:'100%',opacity:0}, {left:0,opacity:1,ease:Power3.easeOut}, '-=0.1')
+			.play();
+	});
+
+	// Prev/Next slide animation : 
+	$('body').on('click', '.arrows a', function(e) {
+		e.preventDefault();
+
+		if ($(this).hasClass('right')) {
+			var right = true;
+		}
+
+		var currentSlideIndex = $('.slide-wrap').slick('slickCurrentSlide');
+		var currentSlide = $('.slide:eq('+currentSlideIndex+')');
+
+		image = currentSlide.find('img');
+		text = currentSlide.find('h4, p');
+
+		beforeChangeTl
+			.to(text, 0.25, {y:50,opacity:0,ease:Expo.easeIn})
+			.to(image, 0.35, {left:'100%', opacity:0,ease:Power3.easeIn,onComplete:function(){
+				if (right === true) {
+					$('.slide-wrap').slick('slickNext');
+				} else {
+					$('.slide-wrap').slick('slickPrev');
+				}
+			}}, "-=0.1")
+			.play();
+	});
+	$('.slide-wrap').on('afterChange', function(event, slick, currentSlide){
+		var slide = $('.slide:eq('+currentSlide+')');
+
+		image = slide.find('img');
+		text = slide.find('h4, p');
+
+		afterChangeTl
+			.staggerFromTo(text, 0.25, {y:50,opacity:0}, {y:0,opacity:1,ease:Expo.easeOut}, 0.1)
+			.fromTo(image, 0.35, {left:'100%',opacity:0}, {left:0,opacity:1,ease:Power3.easeOut}, '-=0.1')
+			.play();
+	});
+
+
+	// Init slider
+	$('.slide-wrap').slick({
+		arrows: false,
+		fade: true
+	});
 }
